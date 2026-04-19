524f83ccf9259c93276eaa7f37a4a130c2aebea7
te0r1ty <max.polyakov@inbox.ru>
2026-04-19 03:00:04 +0300

    Updates and adjustments
    - Completed footer
    - Updated home page
    - Updated search page
    - Updated localisation
    - Configured mailing system
    - Configured submition system
    - Adjusted fonts

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -13,14 +13,19 @@ import {
   AppConfig,
 } from 'src/config/app-config.interface';
 
+
+import { environment } from '../../environments/environment';
+import { PaginationService } from '../core/pagination/pagination.service';
+import { SearchConfigurationService } from '../core/shared/search/search-configuration.service';
 import { Site } from '../core/shared/site.model';
+import { SortDirection } from '../core/cache/models/sort-options.model';
 import { SuggestionsPopupComponent } from '../notifications/suggestions/popup/suggestions-popup.component';
 import { ThemedConfigurationSearchPageComponent } from '../search-page/themed-configuration-search-page.component';
 import { ThemedSearchFormComponent } from '../shared/search-form/themed-search-form.component';
 import { HomeCoarComponent } from './home-coar/home-coar.component';
 import { ThemedHomeNewsComponent } from './home-news/themed-home-news.component';
 import { RecentItemListComponent } from './recent-item-list/recent-item-list.component';
-import { ThemedTopLevelCommunityListComponent } from './top-level-community-list/themed-top-level-community-list.component';
+// import { ThemedTopLevelCommunityListComponent } from './top-level-community-list/themed-top-level-community-list.component';
 
 @Component({
   selector: 'ds-base-home-page',
@@ -34,7 +39,7 @@ import { ThemedTopLevelCommunityListComponent } from './top-level-community-list
     ThemedConfigurationSearchPageComponent,
     ThemedHomeNewsComponent,
     ThemedSearchFormComponent,
-    ThemedTopLevelCommunityListComponent,
+    // ThemedTopLevelCommunityListComponent,
     TranslateModule,
   ],
 })
@@ -46,7 +51,9 @@ export class HomePageComponent implements OnInit {
 
   constructor(
     @Inject(APP_CONFIG) protected appConfig: AppConfig,
+    public searchConfigurationService: SearchConfigurationService,
     protected route: ActivatedRoute,
+    private paginationService: PaginationService,
   ) {
     this.recentSubmissionspageSize = this.appConfig.homePage.recentSubmissions.pageSize;
     this.showDiscoverFilters = this.appConfig.homePage.showDiscoverFilters;
@@ -58,4 +65,11 @@ export class HomePageComponent implements OnInit {
     );
   }
 
+  onLoadMore(): void {
+    this.paginationService.updateRouteWithUrl(this.searchConfigurationService.paginationID, ['search'], {
+      sortField: environment.homePage.recentSubmissions.sortField,
+      sortDirection: 'DESC' as SortDirection,
+    page: 1,
+    });
+  }
 }
